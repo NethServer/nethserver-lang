f0bcb123d9dd846df93e4fbf04d5a715d54acdd1
Davide Principi <davide.principi@nethesis.it>
2015-05-18 13:06:22 +0200

    Pulled de fr pt translations

--- locale/de/server-manager/NethServer_Module_FirewallRules.php
+++ locale/de/server-manager/NethServer_Module_FirewallRules.php
@@ -2,32 +2,32 @@
 
 /* NethServer_Module_FirewallRules translation, language: en */
 
-$L['FirewallRules_Description'] = 'Manage firewall rules';
-$L['FirewallRules_Tags'] = 'firewall rule policy';
-$L['FirewallRules_Title'] = 'Firewall rules';
+$L['FirewallRules_Description'] = 'Verwalte Firewallregeln';
+$L['FirewallRules_Tags'] = 'firewall regel regel';
+$L['FirewallRules_Title'] = 'Firewallregeln';
 
-$L['general_header'] = 'General';
-$L['General_Title'] = 'General options';
-$L['ExternalPing_label'] = 'Ping from Internet';
-$L['permissive_label'] = 'Allowed';
+$L['general_header'] = 'Allgemein';
+$L['General_Title'] = 'Allgemeine Optionen';
+$L['ExternalPing_label'] = 'Ping vom Internet';
+$L['permissive_label'] = 'Erlaubt';
 $L['Policy_label'] = 'Traffic to Internet (red interface)';
-$L['strict_label'] = 'Blocked';
-$L['enabled_label'] = 'Enabled';
-$L['disabled_label'] = 'Disabled';
+$L['strict_label'] = 'Gesperrt';
+$L['enabled_label'] = 'aktiviert';
+$L['disabled_label'] = 'deaktiviert';
 $L['MACValidation_label'] = 'MAC validation (IP/MAC binding)';
 $L['MACValidationPolicy_label'] = 'Policy for hosts without IP/MAC binding (DHCP reservation)';
-$L['drop_label'] = 'Block traffic';
-$L['accept_label'] = 'Allow trafic';
+$L['drop_label'] = 'Gesperrter Verkehr';
+$L['accept_label'] = 'Erlaube Verkehr';
 
 $L['Edit_header'] = 'Edit rule #${0}';
-$L['status_label'] = 'Enabled';
-$L['Action_label'] = 'Action';
+$L['status_label'] = 'aktiviert';
+$L['Action_label'] = 'Aktion';
 $L['ActionReject_label'] = 'Reject';
-$L['ActionAccept_label'] = 'Accept';
-$L['ActionDrop_label'] = 'Drop';
-$L['Source_label'] = 'Source';
-$L['Destination_label'] = 'Destination';
-$L['Service_label'] = 'Service';
+$L['ActionAccept_label'] = 'Akzeptieren';
+$L['ActionDrop_label'] = 'Verwerfen';
+$L['Source_label'] = 'Quelle';
+$L['Destination_label'] = 'Ziel';
+$L['Service_label'] = 'Dienst';
 $L['PickSource_label'] = 'Pick one...';
 $L['PickDestination_label'] = 'Pick one...';
 $L['PickService_label'] = 'Pick one...';
@@ -42,13 +42,13 @@ $L['Create_first_label'] = 'Create rule at top';
 $L['Create_header'] = 'Create firewall rule';
 $L['Copy_header'] = 'Create a copy of #${0}';
 $L['Commit_label'] = 'Apply changes';
-$L['Edit_label'] = 'Edit';
-$L['Copy_label'] = 'Copy';
-$L['Delete_label'] = 'Delete';
-$L['Index_header'] = 'Firewall rules';
+$L['Edit_label'] = 'ändern';
+$L['Copy_label'] = 'Kopieren';
+$L['Delete_label'] = 'löschen';
+$L['Index_header'] = 'Firewallregeln';
 $L['RuleText_label'] = 'From ${Src} to ${Dst}, service ${Service}';
 $L['RuleId_label'] = '#${0}';
-$L['SearchPlaceholder_label'] = 'Search...';
+$L['SearchPlaceholder_label'] = 'Suchen...';
 
 $L['Delete_header'] = 'Delete rule #${0}';
 $L['Delete_message'] = 'Confirm deletion of firewall rule #${id}';
@@ -59,7 +59,7 @@ $L['Services_create'] = 'Create service "${0}"';
 $L['IpRange_create'] = 'Create IP range "${0}"';
 $L['Cidr_create'] = 'Create CIDR network "${0}"';
 
-$L['A_new_rule_label'] = "New rule";
+$L['A_new_rule_label'] = "Neue Regel";
 $L['Any_label'] = 'Any';
 $L['NoRulesDefined_label'] = 'No rules are defined. Create the first one now!';
 
@@ -67,11 +67,11 @@ $L['any_service_label'] = 'any service';
 $L['any_src_dst_label'] = 'any host';
 $L['all_label'] = 'any';
 $L['host_label'] = 'host';
-$L['zone_label'] = 'zone';
-$L['iprange_label'] = 'IP range';
-$L['cidr_label'] = 'CIDR network';
-$L['host-group_label'] = 'group';
-$L['role_label'] = 'interface';
+$L['zone_label'] = 'Zone';
+$L['iprange_label'] = 'IP Bereich';
+$L['cidr_label'] = 'CDIR Netzwerk';
+$L['host-group_label'] = 'Gruppe';
+$L['role_label'] = 'Schnittstelle';
 $L['confirm_reload_label'] = 'No change has been applied.';
 
 $L['FirewallObject_any_Title'] = 'Any';
@@ -82,7 +82,7 @@ $L['FirewallObject_role_Title'] = 'Role ${key}';
 $L['FirewallObject_zone_Title'] = 'Zone ${key}';
 $L['FirewallObject_host-group_Title'] = 'Host group ${key}';
 $L['FirewallObject_fwservice_Title'] = 'Service ${key}';
-$L['FirewallObject_iprange_Title'] = 'IP range ${key}';
+$L['FirewallObject_iprange_Title'] = 'IP Range ${key}';
 $L['FirewallObject_cidr_Title'] = 'CIDR network ${key}';
 
 $L['Show_x_outof_y_label'] = 'Showing ${partial} results out of ${total}';
