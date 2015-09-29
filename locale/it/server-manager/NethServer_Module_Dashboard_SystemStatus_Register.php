808e5216ae5c29664ad50b7c03cdb8fd8da2f73d
Davide Principi <davide.principi@nethesis.it>
2015-09-29 14:24:53 +0200

    Purged Italian translation for Register

--- locale/it/server-manager/NethServer_Module_Dashboard_SystemStatus_Register.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$L['register_title'] = 'Registrazione server';
-$L['serverid_label'] = 'Codice Server';
-$L['product_label'] = 'Prodotti installati';
-$L['not_registered_label'] = 'Server non registrato';
