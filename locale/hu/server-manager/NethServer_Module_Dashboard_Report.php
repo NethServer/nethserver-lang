297916ac30e9cc6c2f5e671de3cdb5efee504d29
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2015-10-13 08:56:30 +0200

    Remove old unused files

--- locale/hu/server-manager/NethServer_Module_Dashboard_Report.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$L['weeks_num_label'] = '#';
-$L['years_label'] = 'Év';
-$L['weeks_label'] = 'Hét';
-$L['Report_Title'] = 'Heti jelentés';
-$L['report_Header'] = 'Heti jelentés';
-$L['Genera_report_label'] = 'Elkészít';
-$L['Empty_report_text'] = 'Nincs készen heti jelentés. Kattints az <b>Elkészít</b>-re a létrehozáshoz';
--- locale/nl/server-manager/NethServer_Module_Dashboard_Report.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$L['weeks_num_label'] = 'n°';
-$L['years_label'] = 'Jaar';
-$L['weeks_label'] = 'Week';
-$L['Report_Title'] = 'Weekly report';
-$L['report_Header'] = 'Weekly report';
-$L['Genera_report_label'] = 'Generate';
-$L['Empty_report_text'] = 'There is no weekly report. Click <b>Generate</b> to create it.';
