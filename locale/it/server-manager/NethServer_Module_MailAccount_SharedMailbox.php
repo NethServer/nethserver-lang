c197da9fef427a0419807f58b9143a189e1faa07
Stefano Fancello <stefano.fancello@nethesis.it>
2016-07-01 18:04:30 +0200

    Add labels and help for automatic alias creation in shared mailbox

---
+++ locale/it/server-manager/NethServer_Module_MailAccount_SharedMailbox.php
@@ -0,0 +1,14 @@
+<?php
+
+$L['SharedMailbox_create_header'] = 'Crea una nuova mailbox condivisa';
+$L['SharedMailbox_modify_header'] = 'Modifica la mailbox condivisa `${0}`';
+$L['Name_label'] = 'Nome';
+$L['Key_label'] = 'Mailbox';
+$L['OwnersSelector_label'] = 'Proprietari';
+$L['Owners_label'] = 'Proprietari';
+$L['Others_label'] = 'Accessi speciali';
+$L['CreateAlias_label'] = 'Crea alias';
+$L['alias_label'] = 'Alias';
+$L['ExtraFields_label'] = 'Azioni aggiuntive';
+$L['ANY_DOMAIN'] = '[tutti i domini]';
+
