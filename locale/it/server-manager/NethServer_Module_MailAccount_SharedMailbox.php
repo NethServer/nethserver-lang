282c990d6df234c75bc4e37b287568dd74c7c736
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2016-09-14 12:11:41 +0200

    Add missing mail and ndpi resources

--- locale/it/server-manager/NethServer_Module_MailAccount_SharedMailbox.php
+++ locale/it/server-manager/NethServer_Module_MailAccount_SharedMailbox.php
@@ -1,14 +1,15 @@
 <?php
 
-$L['SharedMailbox_create_header'] = 'Crea una nuova mailbox condivisa';
-$L['SharedMailbox_modify_header'] = 'Modifica la mailbox condivisa `${0}`';
+$L['SharedMailbox_create_header'] = 'Crea una nuova casella di posta condivisa';
+$L['SharedMailbox_modify_header'] = 'Modifica la casella condivisa `${0}`';
 $L['Name_label'] = 'Nome';
-$L['Key_label'] = 'Mailbox';
+$L['Key_label'] = 'Casella email';
 $L['OwnersSelector_label'] = 'Gruppi proprietari';
 $L['Owners_label'] = 'Gruppi proprietari';
-$L['Others_label'] = 'Accessi speciali';
+$L['Others_label'] = 'Accesso speciale';
 $L['CreateAlias_label'] = 'Crea alias';
 $L['alias_label'] = 'Alias';
 $L['ExtraFields_label'] = 'Azioni aggiuntive';
 $L['ANY_DOMAIN'] = '[tutti i domini]';
-
+$L['localAddress_label'] = 'Indirizzo locale';
+$L['valid_email,malformed-localpart'] = 'indirizzo email non valido';
