24c011840b7f37128b32445873a060cf916167fc
Davide Principi <davide.principi@nethesis.it>
2016-10-20 16:25:13 +0200

    Add NethServer_Module_Bandwidthd.php catalog

---
+++ locale/it/server-manager/NethServer_Module_Bandwidthd.php
@@ -0,0 +1,8 @@
+<?php
+
+$L['Bandwidthd_Description'] = 'Mostra i grafici per l\'analisi dell\'utilizzo della banda';
+$L['Bandwidthd_Tags'] = 'statistiche grafico grafici bandwidthd analisi';
+$L['Bandwidthd_Title'] = 'Utilizzo banda';
+$L['Settings_title'] = 'Impostazioni';
+$L['Settings_label'] = 'Impostazioni';
+$L['Subnets_label'] = 'Raccogli i dati dalle interfacce';
